7704cbc36c93ef68900e28d9ac620da20a94a7a0
lzccheng <1187350675@qq.com>
2018-05-08 10:50:09 +0800

    perfect

--- js/lzcDate.js
+++ js/lzcDate.js
@@ -22,12 +22,13 @@ window.lzcDatePlugin = (function(){
 			//生成标题
 			lzcBox.innerHTML = this.createTitle();
 			//生成星期
-			var lzcDayArr = ['日','一','二','三','四','五','六'];
+			
 			var lzcDay = lzcBox.querySelector('.lzcDay');
-			lzcDay.innerHTML = this.createDay(lzcDayArr);
+			lzcDay.innerHTML = this.createDay();
 			//生成月份
 			var lzcMonth = lzcBox.querySelector('.lzcMonth');
-			for(var c=0;c<6;c++){
+			var monthTotel = 6;
+			for(var c=0;c<monthTotel;c++){
 				var dd = new Date();
 				var dateStr = dd.getFullYear() + '/' + (dd.getMonth() + 1 + c) + '/01';
 				var lzcMonthChild = document.createElement('div');
@@ -41,47 +42,50 @@ window.lzcDatePlugin = (function(){
 				(function(d){
 					if(lzcTdEl[d].innerText){
 						lzcTdEl[d].setAttribute('dataindex',d);
-						lzcTdEl[d].addEventListener('click',function(){
-							var dataTime = this.getAttribute('datatime');
-							console.log(that.elObj)
-							if(!that.elObj.firstEl){
-								that.elObj.firstEl = this;
-								that.addClass(this,'lzcActive');
-							}else{
-								if(Number(this.getAttribute('dataindex'))<Number(that.elObj.firstEl.getAttribute('dataindex'))){
-									
-									that.removeClass(that.elObj.firstEl,'lzcActive');
-									that.elObj.firstEl = this;
-									that.addClass(this,'lzcActive');
-								}else{
-									if(that.elObj.firstEl !== this&&!that.elObj.secondEl){
-										that.elObj.secondEl = this;
-										that.addClass(this,'lzcActive2');
-
-										console.log(Number(that.elObj.firstEl.getAttribute('dataindex')),Number(that.elObj.secondEl.getAttribute('dataindex')))
-										for(var h=Number(that.elObj.firstEl.getAttribute('dataindex')) + 1;h<Number(that.elObj.secondEl.getAttribute('dataindex'));h++){
-											if(lzcTdEl[h].innerText){
-												that.addClass(lzcTdEl[h],'lzcActiveCenter');
-											}
-										}
-										
-									}
-								}
-								
-							}
-							// if(Number(this.getAttribute('datastatus'))){
-							// 	that.removeClass(this,'lzcActive');
-							// 	that.selectArr.splice(that.selectArr.indexOf(dataTime),1);
-							// 	this.setAttribute('datastatus','0');
-							// }else{
-							// 	that.addClass(this,'lzcActive');
-							// 	that.selectArr.push(dataTime);
-							// 	this.setAttribute('datastatus','1');
-							// }
-						});
+						lzcTdEl[d].addEventListener('click', onTdClick);
 					}
 				})(d);
-				
+			}
+			//td->click
+			function onTdClick(){
+				var dataTime = this.getAttribute('datatime');
+				if(that.elObj.secondEl){
+					onTdRemoveClass();
+				}
+				if(!that.elObj.firstEl){
+					that.elObj.firstEl = this;
+					that.addClass(this,'lzcActive');
+				}else{
+					if(Number(this.getAttribute('dataindex'))<Number(that.elObj.firstEl.getAttribute('dataindex'))){
+						that.removeClass(that.elObj.firstEl,'lzcActive');
+						that.elObj.firstEl = this;
+						that.addClass(this,'lzcActive');
+					}else{
+						if(that.elObj.firstEl !== this&&!that.elObj.secondEl){
+							that.elObj.secondEl = this;
+							that.addClass(this,'lzcActive2');
+
+							for(var h=Number(that.elObj.firstEl.getAttribute('dataindex')) + 1;h<Number(that.elObj.secondEl.getAttribute('dataindex'));h++){
+								if(lzcTdEl[h].innerText){
+									that.addClass(lzcTdEl[h],'lzcActiveCenter');
+								}
+							}
+						}
+					}
+					
+				}
+			}
+			//td->移除class
+			function onTdRemoveClass(){
+				for(var h=Number(that.elObj.firstEl.getAttribute('dataindex')) + 1;h<Number(that.elObj.secondEl.getAttribute('dataindex'));h++){
+					if(lzcTdEl[h].innerText){
+						that.removeClass(lzcTdEl[h],'lzcActiveCenter');
+					}
+				};
+				that.removeClass(that.elObj.firstEl,'lzcActive');
+				that.removeClass(that.elObj.secondEl,'lzcActive2');
+				that.elObj.firstEl = null;
+				that.elObj.secondEl = null;
 			}
 			//添加确定按钮
 			var lzcSureBtn = document.createElement('div');
@@ -89,14 +93,61 @@ window.lzcDatePlugin = (function(){
 			lzcSureBtn.innerHTML = '<button class="lzcSureBtn">确定</button>'
 			lzcMonth.appendChild(lzcSureBtn);
 			//给确定按钮添加事件
-			lzcBox.querySelector('.lzcSureBtn').addEventListener('click',function(){
-				that.obj.callback && that.obj.callback(that);
-			})
+			lzcBox.querySelector('.lzcSureBtn').addEventListener('click',onBtnClick);
+			function onBtnClick(){
+				var lzcTip = lzcBox.querySelector('.lzcTip');
+				if(!that.elObj.firstEl){
+					onTip(lzcTip,'请选择入住日期');
+				}
+				if(that.elObj.firstEl&&!that.elObj.secondEl){
+					onTip(lzcTip,'请选择离店日期');
+				}
+				if(that.elObj.secondEl){
+					var data = [];
+					var date1 = new Date(Number(that.elObj.firstEl.getAttribute('datatime')));
+					var obj1 = {
+						dateTime: Number(that.elObj.firstEl.getAttribute('datatime')),
+						date: date1.getFullYear()+'/'+(date1.getMonth()+1)+'/'+date1.getDate(),
+					};
+					var date2 = new Date(Number(that.elObj.secondEl.getAttribute('datatime')));
+					var obj2 = {
+						dateTime: Number(that.elObj.secondEl.getAttribute('datatime')),
+						date: date2.getFullYear()+'/'+(date2.getMonth()+1)+'/'+date2.getDate(),
+					};
+					data.push(obj1);
+					data.push(obj2);
+					that.obj.callback && that.obj.callback(data);
+					lzcBox.style.display = 'none';
+				}
+			}
+			//tip
+			function onTip(dom,text){
+				dom.children[0].innerHTML = text;
+				dom.style.opacity = 1;
+				setTimeout(function(){
+					dom.style.opacity = 0;
+				},3000);
+			}
+			//给el添加点击事件
+			that.el.addEventListener('click',onElClick);
+			function onElClick(){
+				lzcBox.style.display = 'block';
+			}
+			//给lzcIcon添加点击事件
+			lzcBox.querySelector('.lzcIcon').addEventListener('click',onLzcIconClick);
+			function onLzcIconClick(){
+				lzcBox.style.display = 'none';
+			}
+			//填入lzcBox
 			elParent.appendChild(lzcBox);
 		},
 		createTitle: function(){
 			var lzcTitle = this.obj.title || '选择日期';
 			var lzcBoxHtml = '<div class="lzcDateBox">' +
+								'<div class="lzcTip">' +
+									'<span>' +
+									'</span>' +
+								'</div>' + 
 								'<div class="lzcFixed">' +
 									'<div class="lzcTitle">' +
 										'<span class="lzcIcon">&times;</span>'+
@@ -110,7 +161,8 @@ window.lzcDatePlugin = (function(){
 							'</div>'; 
 			return lzcBoxHtml;
 		},
-		createDay: function(lzcDayArr){
+		createDay: function(){
+			var lzcDayArr = ['日','一','二','三','四','五','六'];
 			var lzcDayHtml = '';
 			for(var i=0;i<lzcDayArr.length;i++){
 				lzcDayHtml += '<span class="lzcDayItem">' + lzcDayArr[i] + '</span>';
